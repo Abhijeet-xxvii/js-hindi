f6601c9187c66fab800ec38ec43a16bfb1ee3e87
Abhijeet Gond <abhijeetg2736@gmail.com>
2026-05-15 05:12:30 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // traet all js code as newer version
+
+// alert(2+6)  we are using node js not browser
+
+console.log(3+
+    3)  // code readability must be high
+
+console.log("abhi")
+
+let name = "abhi"
+let age = 10
+let isLoggedIn = false
+
+// number =. 2 to power 52
+//bigint
+//string =""
+//boolean = true/false
+//null = standalone value
+//undefined = 
+//symbol = unique
+
+
+//object 
+
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null)  //object
