728ddb9a3e02b6b77a7be6aba16186f3494211b7
Abhijeet Gond <abhijeetg2736@gmail.com>
2026-05-19 05:00:39 +0000

    video9 summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// primitive data type (call by value)
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol(make component unique), BigInt
+
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const temp = null;
+// let userEmail = undefined;  or let userEmail;
+// const id = Symbol('123');
+// const anotherid = Symbol('123');
+
+// console.log(id==anotherid);
+
+// const bigNumber = 5154484532165484531534845;
+
+
+
+// reference type (Non-primitive dat type)
+
+// Array(type function), Objects(type function), Functions(type object function)
+
+// const hero = ["super","bat","iron","captain"]
+// let myObj = {
+    // name:"abc",
+    // age:12
+// }
+
+// const myFunction = function(){
+// console.log("hello world");
+    
+// }
+
+// console.log(typeof myFunction);
