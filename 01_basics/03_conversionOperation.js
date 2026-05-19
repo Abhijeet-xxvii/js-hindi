fa49040db099264e5d05c2a1d20fd1d10ba1f180
Abhijeet Gond <abhijeetg2736@gmail.com>
2026-05-19 04:18:11 +0000

    abc

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,7 +16,7 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 = true
 // 0 = false
@@ -25,5 +25,40 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let StringNumber = String(someNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// +++++++++++++++     OPERATIONS     +++++++++++++++++
+
+let value =3
+let negValue =-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "abcd"
+
+let str3 = str1 +str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)     = 1
+// console.log(+"")     = 0 
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
